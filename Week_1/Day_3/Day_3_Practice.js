589a374197d53c8d41e299b78e523575f9c10d6b
unknown <shakyadeepankar002@gmail.com>
2026-05-08 12:17:51 +0545

    Day 3 practice

---
+++ Week_1/Day_3/Day_3_Practice.js
@@ -0,0 +1,13 @@
+let text = "JavaScript is Awesome!";
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLocaleLowerCase());
+console.log(text.includes("is"));
+console.log(text.startsWith("Java"));
+
+let name = " Deepankar Shakya ";
+console.log(name.trim());
+console.log(name.charAt(1));
+console.log(name.indexOf("S"));
+console.log(name.slice(0, 10));
+console.log(name.replace("Deep", "JavaScript"));
