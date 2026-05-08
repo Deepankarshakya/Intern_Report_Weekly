02c60cdbd026e3cec1a1d7945971bb0268a9cae5
unknown <shakyadeepankar002@gmail.com>
2026-05-08 11:43:09 +0545

    Day 2 task

--- Week_1/Day_2/Day_2.js
+++ Week_1/Day_2/Day_2.js
@@ -4,4 +4,6 @@ function converter(celcus, franight){
     let c = (franight -32) * 5/9;
     return celcus + "^C celcus into farnight = " + f +" franight \n" + franight + "^F farnight into celcus = " + c + " celcus";
 }
-console.log(converter(0, 3));
+console.log(converter(25, 98.6));
+console.log(converter(-40, 0));
+console.log(converter(3, 3));
