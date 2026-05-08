fa869f53e32eb6d0a4f9efc39d0e081e84770bdd
unknown <shakyadeepankar002@gmail.com>
2026-05-08 11:50:38 +0545

    Day 2 challange_2

---
+++ Week_1/Day_2/challange_2.js
@@ -0,0 +1,8 @@
+let x = 5;
+let y = 10;
+
+function maths(){
+    return "Add = " + (x + y) + "\nSubstraction = " + (x - y) + "\nMultipacation = " + (x *y) + "\nDivide = " + (x / y) + "\nReminder = " + (x % y);
+}
+
+console.log(maths());
