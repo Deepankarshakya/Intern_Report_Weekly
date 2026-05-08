88b3c503e1e43717c08dac988023799d3f5f0a81
unknown <shakyadeepankar002@gmail.com>
2026-05-08 12:55:41 +0545

    Day 3 Challange_4

---
+++ Week_1/Day_4/challange_4.js
@@ -0,0 +1,30 @@
+// This a question to print different grade of student according to marks
+// Which is:
+// 100 - 95 = A+
+// 94 - 90 = A
+// 89 - 85 = A-
+// 84 - 80 = B+
+// 79 - 75 = B
+// 74 - 70 = B-
+// 69 - 65 = C+
+// 64 - 60 = C
+// 59 - 55 = C-
+// 54 - 50 = D
+// 49 - 45 = D-
+// below 45 = Fail
+
+function Calculator(Grade){
+    if(Grade>=95 && Grade <= 100) return "Grade : A+";
+    if(Grade<95 && Grade >= 90) return "Grade : A";
+    if(Grade<90 && Grade >= 85) return "Grade : A-";
+    if(Grade<85 && Grade >= 80) return "Grade : B+";
+    if(Grade<80 && Grade >= 75) return "Grade : B";
+    if(Grade<75 && Grade >= 70) return "Grade : B-";
+    if(Grade<70 && Grade >= 65) return "Grade : C+";
+    if(Grade<65 && Grade >= 60) return "Grade : C";
+    if(Grade<60 && Grade >= 55) return "Grade : C-";
+    if(Grade<55 && Grade >= 50) return "Grade : D";
+    if(Grade<50 && Grade >= 45) return "Grade : D-";
+    else "Fail";
+}
+console.log(Calculator(45));
