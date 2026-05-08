76df5d4fd9e5e625ec44838c01d46599c7f86dc3
unknown <shakyadeepankar002@gmail.com>
2026-05-08 11:05:02 +0545

    Fixed folder structure and added Day 1 files properly

---
+++ Week_1/Day_1/challange_1.js
@@ -0,0 +1,13 @@
+const name = "Deepankar Shakya";
+const age = 24;
+const place = "Lalitpur-6, Thapahity";
+const degree = "I have finished my blachelor in BCA";
+const extra = "I have completed N4 and N5 level of Japanese Language, and I have my N5 degree of JLPT";
+const hobby = "My hobby was to play Games."
+
+function myDetail(){
+    let me = `My name is ${name}, I am ${24} years old. \n I live in ${place}. ${degree}
+    ${extra}. ${hobby}`;
+    return me;
+}
+console.log(myDetail());
---
+++ Week_1/Day_1/day_1.js
@@ -0,0 +1,30 @@
+const mySelf = [
+    {name: "Deepankar", age: 24, place: "Thapahity, Lalitpur-6"},
+    {name: "Deep", age: 24, place: "Thapahity, Lalitpur-4"},
+    {name: "Deepak", age: 24, place: "Thapahity, Lalitpur-2"},
+    {name: "Deepa", age: 24, place: "Thapahity, Lalitpur-1"}
+];
+function me(){
+    let Deepankar = "";
+    for(let i = 0; i < mySelf.length; i++){
+       Deepankar += "My name is " + mySelf[i].name + ". " + "I am " + mySelf[i].age + " years old. " + "I live in " + mySelf[i].place + ".\n";
+    }
+    return Deepankar;
+}
+console.log(me());
+
+// First Task
+console.log("Hello, JavaScript. \n");
+console.log("My name is Deepankar Shakya \n");
+console.log("I'm learning JavaScript. \n");
+
+//Second Task 
+const name = "Deepankar Shakya";
+const age = 24;
+const address = "Lalitpur-6, Thapahity.";
+function details(){
+    let full = `Name: ${name}, age: ${age}, address: ${address}`;
+    return full;
+}
+console.log(details());
+
